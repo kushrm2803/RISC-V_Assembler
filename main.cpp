77ce763e8d821ea249e33431dc292b38ae8b6104
kushrm2803 <mahajankush2803@gmail.com>
2024-03-10 00:51:11 +0530

    Added comments

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int text_address;
 int data_address;
 int mode; // 0 -> .text && 1 -> .data
 
+// Function to tokenize the instruction string
 vector<string> instructionToToken(string instruct)
 {
     vector<string> token;
@@ -28,12 +29,14 @@ vector<string> instructionToToken(string instruct)
         while (j < len && instruct[j] != ' ' && instruct[j] != ',')
         {
             if (instruct[j] == '#')
-            { // comments case
+            {
+                // Handle comments
                 flag = true;
                 break;
             }
             if (instruct[j] == ':' && mode == 0)
-            { // labeling in case of text-mode
+            {
+                // Handle labeling in text-mode
                 mp[word] = text_address;
                 word.erase();
                 j++;
@@ -45,35 +48,36 @@ vector<string> instructionToToken(string instruct)
         if (word.length() > 0)
             token.push_back(word);
         if (flag)
-            break; // the next lines are comment
+            break; // Ignore comments
     }
     return token;
 }
 
 int main(int argc, char *argv[])
 {
-    defineAllCodes();
-    mode = 0; // intial mode is text
+    defineAllCodes(); // Define all instruction opcodes
+    mode = 0;         // Initial mode is text
     text_address = 0;
-    // argv[0] is ./a.exe
+    // Loop through command line arguments
     for (int i = 1; i <= argc; i++)
     {
-        string in = chartostring(argv[i]);
-        string out = outputfilename(in);
+        string in = chartostring(argv[i]); // Input file name
+        string out = outputfilename(in);   // Output file name
         string datadata;
         ifstream fin;
         ofstream fout;
         fin.open(in);
         fout.open(out);
         string instruct;
-        // for generating all address of instructions
+        // Generate addresses of instructions
         while (getline(fin, instruct))
         {
             vector<string> tokens = instructionToToken(instruct);
             if (tokens.empty())
                 continue;
             if (mode == 0)
-            { // text-mode
+            {
+                // Text-mode
                 if (tokens[0] == ".data")
                 {
                     mode = 1;
@@ -85,6 +89,7 @@ int main(int argc, char *argv[])
             }
             else
             {
+                // Data-mode
                 if (tokens[0] == ".text")
                 {
                     mode = 0;
@@ -95,17 +100,19 @@ int main(int argc, char *argv[])
             }
         }
         fin.close();
+        // Reset addresses
         fin.open(in);
         text_address = 0;
         data_address = 268435456;
-        // main code starts here
+        // Generate machine code
         while (getline(fin, instruct))
         {
             vector<string> tokens = instructionToToken(instruct);
             if (tokens.empty())
                 continue;
             if (mode == 0)
-            { // text-mode
+            {
+                // Text-mode
                 if (tokens[0] == ".data")
                 {
                     mode = 1;
@@ -113,17 +120,18 @@ int main(int argc, char *argv[])
                 }
                 if (tokens[0] == ".text")
                     continue;
-                // 31 instructions code begins
-
-                // R-type
+                // 31 Instructions code begins
+                // R-type instructions
                 if (r_func7.find(tokens[0]) != r_func7.end())
                 {
+                    // Instruction check according to pattern of R instruction
                     if (tokens.size() > 4 || tokens[1].substr(0, 1) != "x" || tokens[2].substr(0, 1) != "x" || tokens[3].substr(0, 1) != "x")
                     {
                         fout << "Error" << endl;
                     }
                     else
                     {
+                        // Extract register and immediate values
                         tokens[1].erase(0, 1);
                         tokens[2].erase(0, 1);
                         tokens[3].erase(0, 1);
@@ -138,18 +146,19 @@ int main(int argc, char *argv[])
                         fout << "0x" << bin_to_hex(dec_to_bin(text_address, 1)) << " 0x" << code << endl;
                     }
                 }
-
-                // I-type
+                // I-type instructions
                 else if (i_func3.find(tokens[0]) != i_func3.end())
                 {
                     if (tokens[0] == "lb" || tokens[0] == "lw" || tokens[0] == "ld" || tokens[0] == "lh")
-                        offset_separate(tokens);
+                        offset_separate(tokens); // Separate offset from base register
+                    // Instruction check according to pattern of I instruction
                     if (tokens.size() > 4 || tokens[1].substr(0, 1) != "x" || tokens[2].substr(0, 1) != "x" || tokens[3].substr(0, 1) == "x")
                     {
                         fout << "Error" << endl;
                     }
                     else
                     {
+                        // Extract register and immediate values
                         tokens[1].erase(0, 1);
                         tokens[2].erase(0, 1);
                         int idest = stoi(tokens[1]);
@@ -159,6 +168,7 @@ int main(int argc, char *argv[])
                         int k = 0;
                         bool neg = false;
                         bool error = false;
+                        // Extract immediate value
                         string imd;
                         string upper_lim = "011111111111";
                         string lower_lim = "100000000000";
@@ -167,6 +177,7 @@ int main(int argc, char *argv[])
                             neg = true;
                             k++;
                         }
+                        // Handle Hexadecimal format of immediate values
                         if (tokens[3].substr(k, 2) == "0x")
                         {
                             tokens[3].erase(0, 2 + k);
@@ -177,6 +188,7 @@ int main(int argc, char *argv[])
                             else
                                 imd = dec_to_bin(hex_to_dec(tokens[3]), 12);
                         }
+                        // Handle Binary format of immediate values
                         else if (tokens[3].substr(k, 2) == "0b")
                         {
                             tokens[3].erase(0, 2 + k);
@@ -187,6 +199,7 @@ int main(int argc, char *argv[])
                             imd = tokens[3];
                             error = valid_check(imd, 2);
                         }
+                        // Handle Decimal format of immediate values
                         else
                         {
                             error = valid_check(tokens[3], 10);
@@ -207,51 +220,47 @@ int main(int argc, char *argv[])
                         }
                     }
                 }
-                // S-type
+                // S-type instructions
                 else if (s_func3.find(tokens[0]) != s_func3.end())
                 {
-                    offset_separate(tokens);
-
+                    offset_separate(tokens); // Separate offset from base register
+                    // Instruction check according to pattern of S instruction
                     if (tokens.size() > 4 || tokens[1].substr(0, 1) != "x" || tokens[2].substr(0, 1) != "x" || tokens[3].substr(0, 1) == "x")
                     {
                         fout << "Error" << endl;
                     }
                     else
                     {
-
+                        // Extract register and offset values
                         tokens[1].erase(0, 1);
                         tokens[2].erase(0, 1);
-
                         int rs2 = stoi(tokens[1]);
                         int rs1 = stoi(tokens[2]);
-
                         string fr2 = dec_to_bin(rs2, 5);
                         string fr1 = dec_to_bin(rs1, 5);
-
                         int k = 0;
                         bool neg = false;
                         bool error = false;
                         string imd;
                         string upper_lim = "011111111111";
                         string lower_lim = "100000000000";
-
                         if (tokens[3][k] == '-')
                         {
                             neg = true;
                             k++;
                         }
+                        // Handle Hexadecimal format of offset values
                         if (tokens[3].substr(k, 2) == "0x")
                         {
                             tokens[3].erase(0, 2 + k);
-
                             error = valid_check(tokens[3], 16);
-
                             int imm = hex_to_dec(tokens[3]);
                             if (neg)
                                 imd = dec_to_bin(pow(2, 12) - abs(imm), 12);
                             else
                                 imd = dec_to_bin(hex_to_dec(tokens[3]), 12);
                         }
+                        // Handle Binary format of offset values
                         else if (tokens[3].substr(k, 2) == "0b")
                         {
                             tokens[3].erase(0, 2 + k);
@@ -260,9 +269,9 @@ int main(int argc, char *argv[])
                                 tokens[3] = "0" + tokens[3];
                             }
                             imd = tokens[3];
-
                             error = valid_check(imd, 2);
                         }
+                        // Handle Decimal format of offset values
                         else
                         {
                             error = valid_check(tokens[3], 10);
@@ -272,10 +281,8 @@ int main(int argc, char *argv[])
                             else
                                 imd = dec_to_bin(imm, 12);
                         }
-
                         string i1 = imd.substr(0, 7);
                         string i2 = imd.substr(7, 5);
-
                         if ((imd > upper_lim && !neg) || (imd < lower_lim && neg) || imd.size() > 12)
                             fout << "0x" << bin_to_hex(dec_to_bin(text_address, 1)) << " Immediate value out of bounds" << endl;
                         else if (error)
@@ -288,9 +295,10 @@ int main(int argc, char *argv[])
                     }
                 }
 
-                // U-type
+                // U-type instructions
                 else if (u_opcode.find(tokens[0]) != u_opcode.end())
                 {
+                    // Instruction check according to pattern of U instruction
                     if (tokens.size() > 3 || tokens[1].substr(0, 1) != "x" || tokens[2].substr(0, 1) == "x")
                     {
                         fout << "Error" << endl;
@@ -310,6 +318,7 @@ int main(int argc, char *argv[])
                             neg = true;
                             tokens[2].erase(0, 1);
                         }
+                        // Handle Hexadecimal format of immediate values
                         if (tokens[2].substr(0, 2) == "0x")
                         {
                             tokens[2].erase(0, 2);
@@ -317,6 +326,7 @@ int main(int argc, char *argv[])
                             int imm = hex_to_dec(tokens[2]);
                             imd = dec_to_bin(hex_to_dec(tokens[2]), 20);
                         }
+                        // Handle Binary format of immediate values
                         else if (tokens[2].substr(0, 2) == "0b")
                         {
                             tokens[2].erase(0, 2);
@@ -327,6 +337,7 @@ int main(int argc, char *argv[])
                             imd = tokens[2];
                             error = valid_check(imd, 2);
                         }
+                        // Handle Decimal format of immediate values
                         else
                         {
                             error = valid_check(tokens[2], 10);
@@ -345,18 +356,21 @@ int main(int argc, char *argv[])
                     }
                 }
 
-                // UJ-format // jal
+                // UJ-type instructions
                 else if (tokens[0] == "jal")
                 {
+                    // Instruction check according to pattern of UJ instruction
                     if (tokens.size() > 3 || tokens[1].substr(0, 1) != "x")
                     {
                         fout << "Error" << endl;
                     }
                     else
                     {
+                        // Calculate the immediate value for jumping
                         int imm = (mp[tokens[2]] - text_address);
                         tokens[1].erase(0, 1);
                         int dest = stoi(tokens[1]);
+                        // Convert immediate value and destination register to binary
                         string imm1 = dec_to_bin(imm, 21);
                         string temp = imm1.substr(10, 10);
                         string temp2 = imm1.substr(1, 8);
@@ -366,21 +380,24 @@ int main(int argc, char *argv[])
                         fout << code << endl;
                     }
                 }
-                // SB format
+                // SB-type instructions
                 else if (sb_func3.find(tokens[0]) != sb_func3.end())
                 {
+                    // Instruction check according to pattern of SB instruction
                     if (tokens.size() > 4 || tokens[1].substr(0, 1) != "x" || tokens[2].substr(0, 1) != "x")
                     {
                         fout << "Error" << endl;
                     }
                     else
                     {
+                        // Extract register values
                         tokens[1].erase(0, 1);
                         tokens[2].erase(0, 1);
                         int ir1 = stoi(tokens[1]);
                         int ir2 = stoi(tokens[2]);
                         string r1 = dec_to_bin(ir1, 5);
                         string r2 = dec_to_bin(ir2, 5);
+                        // Calculate the immediate value for branching
                         int imm = (mp[tokens[3]] - text_address);
                         string imm1 = dec_to_bin(imm, 13);
                         string code = imm1[0] + imm1.substr(2, 6) + r2 + r1 + sb_func3[tokens[0]] + imm1.substr(8, 4) + imm1[1] + "1100011";
@@ -395,11 +412,12 @@ int main(int argc, char *argv[])
                     return 0;
                 }
 
-                // 31 instructions code ends
-                text_address += 4;
+                // 31 Instructions code ends
+                text_address += 4; // Increment text address
             }
             else
-            { // data-mode
+            {
+                // Data-mode
                 if (tokens[0] == ".text")
                 {
                     mode = 0;
@@ -410,6 +428,7 @@ int main(int argc, char *argv[])
                 // data begins
                 if (tokens[2] == ".word" || tokens[2] == ".half" || tokens[2] == ".dword" || tokens[2] == ".byte")
                 {
+                    // Loop through each data value
                     int i = 3;
                     while (tokens[i] != "\0")
                     {
@@ -418,6 +437,7 @@ int main(int argc, char *argv[])
                         bool neg = false;
                         bool error = false;
                         map<string, int> data_size = {{".dword", 16}, {".byte", 2}, {".word", 8}, {".half", 4}};
+                        // Initialize an empty vector to hold memory blocks
                         vector<string> memory_block;
                         if (tokens[i][k] == '-')
                         {
@@ -425,6 +445,7 @@ int main(int argc, char *argv[])
                             k++;
                         }
                         string data = "";
+                        // Determine the format of the input data and convert it to hexadecimal
                         if (tokens[i].substr(k, 2) == "0x")
                         {
                             tokens[i].erase(0, 2 + k);
@@ -462,7 +483,7 @@ int main(int argc, char *argv[])
                         }
                         else
                         {
-                            // All the memory values will be printed in Hexadecimal
+                            // Write memory values in hexadecimal format
                             int j = data_size[tokens[2]] - 2;
                             while (j >= 0)
                             {
@@ -474,21 +495,25 @@ int main(int argc, char *argv[])
                         i++;
                     }
                 }
+                // Processing null-terminated ASCII strings
                 else if (tokens[2] == ".asciiz")
                 {
                     int i = 3;
                     string data = "";
                     int asciiz_len = 0;
+                    // Concatenate each character into a string
                     while (i < tokens.size())
                     {
                         data += tokens[i];
                         data += " ";
                         i++;
                     }
+                    // Remove leading and trailing quotes from the string
                     data.erase(0, 1);
                     data.pop_back();
                     data.pop_back();
                     asciiz_len = data.size();
+                    // Convert each character to hexadecimal and write to the output file
                     for (char c : data)
                     {
                         datadata += "0x" + bin_to_hex(dec_to_bin(data_address, 8)) + " " + bin_to_hex(dec_to_bin((int)c, 0)) + "\n";
@@ -497,7 +522,7 @@ int main(int argc, char *argv[])
                     datadata += "0x" + bin_to_hex(dec_to_bin(data_address, 8)) + " " + "00" + "\n";
                     data_address += 1; // for terminating zero
                 }
-                // data ends
+                // Data ends
             }
         }
         fout << "\n\n------------------------DATA--------------------------\n"
