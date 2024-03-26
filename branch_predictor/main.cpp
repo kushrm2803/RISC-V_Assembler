9566f0a864d212fe51f4222c71d75016224f9546
kushrm2803 <mahajankush2803@gmail.com>
2024-03-26 10:43:00 +0530

    Removed Errors

--- branch_predictor/main.cpp
+++ branch_predictor/main.cpp
@@ -16,8 +16,8 @@ float Always_Taken(int a, int b)
     }
     else
     {
-        float acc = (static_cast<float>(a) / (a + b)) * 100.0;
-        return acc;
+        float acc = (double)a / (double)(a + b);
+        return acc * 100;
     }
 }
 float Always_Not_Taken(int a, int b)
@@ -29,74 +29,92 @@ float Always_Not_Taken(int a, int b)
     }
     else
     {
-        float acc = (static_cast<float>(b) / (a + b)) * 100.0;
-        return acc;
+        float acc = (double)b / (double)(a + b);
+        return acc * 100;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    
-        int Taken_count = 0;
-        int Not_taken_count = 0;
-        for (int i = 1; i < argc; i++)
+
+    int Taken_count = 0;
+    int Not_taken_count = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        ifstream fin;
+        ofstream fout;
+        fin.open(argv[i]);
+        string instruct;
+        // Generate addresses of instructions
+        string next_pc;
+        string branch_pc;
+        map<string, int> history_table; // pc->cur state of instruction
+        map<string, int> oneBitBuffer;
+        map<string, int> twoBitBuffer;
+        int correctPrediction = 0;
+        int wrongPrediction = 0;
+        int correctPrediction1 = 0;
+        int wrongPrediction1 = 0;
+        while (getline(fin, instruct))
         {
-            ifstream fin;
-            ofstream fout;
-            fin.open(argv[i]);
-            string instruct;
-            // Generate addresses of instructions
-            string prev_pc;
-            map<string, int> history_table; // pc->cur state of instruction
-            map<string, int> oneBitBuffer;
-            map<string, int> twoBitBuffer;
-            int correctPrediction = 0;
-            int wrongPrediction = 0;
-            int correctPrediction1 = 0;
-            int wrongPrediction1 = 0;
-            while (getline(fin, instruct))
+            vector<string> tokens = instructionToToken(instruct);
+            if (tokens.empty() || tokens[0].substr(0,2)!="0x")
+                continue;
+            if (branch_pc.size() != 0)
             {
-                vector<string> tokens = instructionToToken(instruct);
-                if (tokens.empty())
-                    continue;
-                if (tokens[2][0] == 'b')
-                {
-                    if (prev_pc.size() != 0)
-                        if (actualBranch(prev_pc, tokens[0]))
-                            history_table[tokens[0]] = 1;
-                        else
-                            history_table[tokens[0]] = 0;
-                    cout << "Branch Target Buffer of 1-bit:" << endl;
-                    if (oneBitBranchPredictor(oneBitBuffer, history_table, tokens))
-                        correctPrediction++;
-                    else
-                        wrongPrediction++;
-                    if(twoBitBuffer.find(tokens[0])==twoBitBuffer.end())twoBitBuffer[tokens[0]]=1; //two bit branch predictor should start from not taken
-                    cout << "Branch Target Buffer of 2-bit:" << endl;
-                    if (twoBitBranchPredictor(twoBitBuffer, history_table, tokens))
-                        correctPrediction1++;
-                    else
-                        wrongPrediction1++;
-                    if (actualBranch(prev_pc, tokens[0]))
-                    {
-                        cout << tokens[0] << "  Taken" << endl;
-                        Taken_count++;
-                    }
-                    else
-                    {
-                        cout << tokens[0] << "  Not Taken" << endl;
-                        Not_taken_count++;
-                    }
-                    prev_pc = tokens[0];
+                if (actualBranch(tokens[0], branch_pc)){
+                    // cout<<branch_pc<<" "<<"Taken"<<endl;
+                    history_table[branch_pc] = 1;
+                    branch_pc.clear();
+                }
+                else{
+                    // cout<<branch_pc<<" "<<"NOTTaken"<<endl;
+                    history_table[branch_pc] = 0;
+                    branch_pc.clear();
                 }
             }
-            cout << "Total Prediction of 1-bit Branch Predictor ->" << ((double)correctPrediction / (double)(correctPrediction + wrongPrediction)) * 100 << "%" << endl;
-            cout << "Total Prediction of 2-bit Branch Predictor ->" << ((double)correctPrediction1 / (double)(correctPrediction1 + wrongPrediction1)) * 100 << "%" << endl;
+            if (tokens[2][0] == 'b')
+            {
+                branch_pc = tokens[0];
+                // continue;
+                // cout<<tokens[2]<<endl;
+                // if (prev_pc.size() != 0)
+                //     if (actualBranch(prev_pc, tokens[0]))
+                //         history_table[tokens[0]] = 1;
+                //     else
+                //         history_table[tokens[0]] = 0;
+                // cout << "Branch Target Buffer of 1-bit:" << endl;
+                if (oneBitBranchPredictor(oneBitBuffer, history_table, tokens))
+                    correctPrediction++;
+                else
+                    wrongPrediction++;
+                if (twoBitBuffer.find(tokens[0]) == twoBitBuffer.end())
+                    twoBitBuffer[tokens[0]] = 1; // two bit branch predictor should start from not taken
+                // // cout << "Branch Target Buffer of 2-bit:" << endl;
+                if (twoBitBranchPredictor(twoBitBuffer, history_table, tokens))
+                    correctPrediction1++;
+                else
+                    wrongPrediction1++;
+                // if (actualBranch(prev_pc, tokens[0]))
+                // {
+                //     // cout << tokens[0] << "  Taken" << endl;
+                //     Taken_count++;
+                // }
+                // else
+                // {
+                //     // cout << tokens[0] << "  Not Taken" << endl;
+                //     Not_taken_count++;
+                // }
+            }
+            // prev_pc = tokens[0];
         }
-        float Accuracy = Always_Taken(Taken_count, Not_taken_count);
-        cout << "accuracy of branch predictor By Always Taken : " << Accuracy << endl;
-
-        float Acc = Always_Not_Taken(Taken_count, Not_taken_count);
-        cout << "accuracy of branch predictor By Always Not Taken : " << Acc << endl;
-        return 0;
+        cout << "Total Prediction of 1-bit Branch Predictor ->" << ((double)correctPrediction / (double)(correctPrediction + wrongPrediction)) * 100 << "%" << endl;
+        cout << "Total Prediction of 2-bit Branch Predictor ->" << ((double)correctPrediction1 / (double)(correctPrediction1 + wrongPrediction1)) * 100 << "%" << endl;
+        // float Accuracy = Always_Taken(Taken_count, Not_taken_count);
+        // cout << "accuracy of branch predictor By Always Taken -> " << Accuracy << "%" << endl;
+        // float Acc = Always_Not_Taken(Taken_count, Not_taken_count);
+        // cout << "accuracy of branch predictor By Always Not Taken -> " << Acc << "%" << endl;
     }
+
+    return 0;
+}
